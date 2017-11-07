85f296d17ceac5350b9eba216e9f1b51fcf1ea31
pedrosbo1 <pedro.ac@puccampinas.edu.br>
2017-11-07 14:51:40 -0200

    melhorando o shell

--- Projeto2/minix/setup.sh
+++ Projeto2/minix/setup.sh
@@ -7,6 +7,9 @@ echo "[-m] para montar"
 echo "[-t] Efetuar todo teste com o mesmo modulo"
 else
 if [ $1 = "-c" ]; then
+make
+sudo rmmod minix
+sudo insmod minix2.ko 
 echo "criador de sistemas de arquivos"
 sudo dd if=/dev/zero of=file.img bs=1k count=10000
 echo "transformando file.img em um arquivo de bloco"
@@ -22,6 +25,7 @@ echo "Efetuando teste com mesmo modulo"
 sudo losetup /dev/loop0 file.img
 sudo umount /dev/loop0
 sudo rmmod minix2
+sudo rmmod minix
 make
 sudo insmod minix2.ko
 sudo mount /dev/loop0 /proj2 
